0e98b0f835278e070eb62b3a5c13b6bbc2e46e9c
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-04-14 08:24:59 -0400

    feat: make inert when ember-source supports default helper manager

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 'use strict';
 
+const VersionChecker = require('ember-cli-version-checker');
+
+const MINIMUM_VERSION = '4.5.0-alpha.3'
+
 module.exports = {
   name: require('./package').name,
+
+  _usePolyfill: flag(process.env.USE_DEFAULT_HELPER_MANAGER_POLYFILL),
 
   options: {
     'ember-cli-babel': {
       enableTypeScriptTransform: true,
     },
   },
+
+  usePolyfill() {
+    if (this._usePolyfill === undefined) {
+      let version = new VersionChecker(this.project).for(`ember-source`);
+      this._usePolyfill = version.lt(MINIMUM_VERSION);
+    }
+
+    return this._usePolyfill;
+  },
+
+  treeForAddon() {
+    if (this.usePolyfill()) {
+      return this._super.treeForAddon.apply(this, arguments);
+    }
+  },
+
+  treeForApp() {
+    if (this.usePolyfill()) {
+      return this._super.treeForApp.apply(this, arguments);
+    }
+  },
 };
+
+const FALSE = ['false', 'disable', 'no', 'off', '0'];
+
+function flag(flag) {
+  if (flag === undefined) {
+    return undefined;
+  } else if (FALSE.includes(flag.toLowerCase())) {
+    return false;
+  } else {
+    return true;
+  }
+}
