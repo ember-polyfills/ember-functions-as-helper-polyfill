8d368e55b5a2c13bf77b5444250602d7659198b4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-06 15:16:35 -0400

    Fix

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,10 +1,15 @@
-import { getSettledState, resetOnerror, setApplication } from '@ember/test-helpers';
+import {
+  getSettledState,
+  resetOnerror,
+  setApplication,
+} from '@ember/test-helpers';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
+import { loadTests } from 'ember-qunit/test-loader';
 
 setApplication(Application.create(config.APP));
 
@@ -12,4 +17,5 @@ setup(QUnit.assert);
 QUnit.testDone(resetOnerror);
 Object.assign(window, { getSettledState });
 
+loadTests();
 start();
